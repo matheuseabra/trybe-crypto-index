0f2a7ef571536e028c75329cc3186f5c16e00c99
Matheus Seabra <matheus@academicmerit.com>
2020-12-09 20:39:48 -0300

    Fix update.spec.ts test

--- client/tests/update.spec.tsx
+++ client/tests/update.spec.tsx
@@ -5,8 +5,8 @@ describe('Update', () => {
   it('should render a update currency page', async () => {
     const { container } = render(<Update />);
     expect(container).toBeTruthy();
-    // expect(screen.getByText('Voltar')).toBeInTheDocument();
-    // expect(screen.getByText('Novo Valor')).toBeInTheDocument();
-    // expect(screen.getByText('ATUALIZAR')).toBeInTheDocument();
+    expect(screen.getByText('Voltar')).toBeInTheDocument();
+    expect(screen.getByText('Novo valor')).toBeInTheDocument();
+    expect(screen.getByText('ATUALIZAR')).toBeInTheDocument();
   });
 });
